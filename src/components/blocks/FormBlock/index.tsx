0fa5bc806607c0cfd5d74e4a2796d545241c104b
AndreiChristopher <33236539+AndreiChristopher@users.noreply.github.com>
2025-03-31 18:50:58 -0300

    <div></div>

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -35,52 +35,54 @@ export default function FormBlock(props) {
     };
 
     return (
-        <form
-            className={classNames(
-                'sb-component',
-                'sb-component-block',
-                'sb-component-form-block',
-                className,
-                styles?.self?.margin ? mapStyles({ margin: styles?.self?.margin }) : undefined,
-                styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined,
-                styles?.self?.borderWidth && styles?.self?.borderWidth !== 0 && styles?.self?.borderStyle !== 'none'
-                    ? mapStyles({
-                          borderWidth: styles?.self?.borderWidth,
-                          borderStyle: styles?.self?.borderStyle,
-                          borderColor: styles?.self?.borderColor ?? 'border-primary'
-                      })
-                    : undefined,
-                styles?.self?.borderRadius ? mapStyles({ borderRadius: styles?.self?.borderRadius }) : undefined
-            )}
-            name={elementId}
-            method="POST" data-netlify="true"
-                <input type="hidden" name="form-name" value={elementId} />
-            id={elementId}
-            //onSubmit={handleFormSubmit}
-            ref={formRef}
-            data-sb-field-path={fieldPath}
-        >
-            <div
-                className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
-                {...(fieldPath && { 'data-sb-field-path': '.fields' })}
-            >
-                {fields.map((field, index) => {
-                    const modelName = field.__metadata.modelName;
-                    if (!modelName) {
-                        throw new Error(`form field does not have the 'modelName' property`);
-                    }
-                    const FormControl = getComponent(modelName);
-                    if (!FormControl) {
-                        throw new Error(`no component matching the form field model name: ${modelName}`);
-                    }
-                    return <FormControl key={index} {...field} {...(fieldPath && { 'data-sb-field-path': `.${index}` })} />;
-                })}
-            </div>
-            {submitButton && (
-                <div className={classNames('mt-8', 'flex', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}>
-                    <SubmitButtonFormControl {...submitButton} {...(fieldPath && { 'data-sb-field-path': '.submitButton' })} />
+        <div>
+            <form
+                className={classNames(
+                    'sb-component',
+                    'sb-component-block',
+                    'sb-component-form-block',
+                    className,
+                    styles?.self?.margin ? mapStyles({ margin: styles?.self?.margin }) : undefined,
+                    styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined,
+                    styles?.self?.borderWidth && styles?.self?.borderWidth !== 0 && styles?.self?.borderStyle !== 'none'
+                        ? mapStyles({
+                              borderWidth: styles?.self?.borderWidth,
+                              borderStyle: styles?.self?.borderStyle,
+                              borderColor: styles?.self?.borderColor ?? 'border-primary'
+                          })
+                        : undefined,
+                    styles?.self?.borderRadius ? mapStyles({ borderRadius: styles?.self?.borderRadius }) : undefined
+                )}
+                name={elementId}
+                method="POST" data-netlify="true"
+                    <input type="hidden" name="form-name" value={elementId} />
+                id={elementId}
+                    //onSubmit={handleFormSubmit}
+                    ref={formRef}
+                    data-sb-field-path={fieldPath}
+                >
+                    <div
+                        className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
+                    {...(fieldPath && { 'data-sb-field-path': '.fields' })}
+                >
+                    {fields.map((field, index) => {
+                        const modelName = field.__metadata.modelName;
+                        if (!modelName) {
+                            throw new Error(`form field does not have the 'modelName' property`);
+                        }
+                        const FormControl = getComponent(modelName);
+                        if (!FormControl) {
+                            throw new Error(`no component matching the form field model name: ${modelName}`);
+                        }
+                        return <FormControl key={index} {...field} {...(fieldPath && { 'data-sb-field-path': `.${index}` })} />;
+                    })}
                 </div>
-            )}
-        </form>
+                {submitButton && (
+                    <div className={classNames('mt-8', 'flex', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}>
+                        <SubmitButtonFormControl {...submitButton} {...(fieldPath && { 'data-sb-field-path': '.submitButton' })} />
+                    </div>
+                )}
+            </form>
+        </div>
     );
 }
