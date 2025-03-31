5a67200404011438042aa9abcd5216372a0536d4
AndreiChristopher <33236539+AndreiChristopher@users.noreply.github.com>
2025-03-31 14:45:36 -0300

    Update components-registry.ts#2

--- src/components/components-registry.ts
+++ src/components/components-registry.ts
@@ -11,7 +11,7 @@ import { ComponentType } from 'react';
 
 /*export function getComponent(key: string): ComponentType {
     return components[key];
-}*/
+}
 
 /**
  * Map of dynamically imported components.
@@ -29,9 +29,8 @@ import { ComponentType } from 'react';
  *
  *     const Section = getComponent(section.__metadata.modelName);
  *     return <Section {...section} />;
- */
-const components: { [key: string]: ComponentType<any> } = {
-//const components = {
+ 
+const components = {
     AutoCompletePosts: dynamic(() => import('./blocks/SearchBlock/AutoCompletePosts')),
     CarouselSection: dynamic(() => import('./sections/CarouselSection')),
     CheckboxFormControl: dynamic(() => import('./blocks/FormBlock/CheckboxFormControl')),
@@ -58,3 +57,42 @@ const components: { [key: string]: ComponentType<any> } = {
     PostFeedLayout: dynamic(() => import('./layouts/PostFeedLayout')),
     PostFeedCategoryLayout: dynamic(() => import('./layouts/PostFeedCategoryLayout'))
 };
+*/
+
+/**
+ * A map of dynamically imported components.
+ */
+const components: { [key: string]: ComponentType<any> } = {
+    AutoCompletePosts: dynamic(() => import('./blocks/SearchBlock/AutoCompletePosts')),
+    CarouselSection: dynamic(() => import('./sections/CarouselSection')),
+    CheckboxFormControl: dynamic(() => import('./blocks/FormBlock/CheckboxFormControl')),
+    DividerSection: dynamic(() => import('./sections/DividerSection')),
+    EmailFormControl: dynamic(() => import('./blocks/FormBlock/EmailFormControl')),
+    FeaturedItem: dynamic(() => import('./sections/FeaturedItemsSection/FeaturedItem')),
+    FeaturedItemToggle: dynamic(() => import('./sections/FeaturedItemsSection/FeaturedItemToggle')),
+    FeaturedItemsSection: dynamic(() => import('./sections/FeaturedItemsSection')),
+    FeaturedPeopleSection: dynamic(() => import('./sections/FeaturedPeopleSection')),
+    FeaturedPostsSection: dynamic(() => import('./sections/FeaturedPostsSection')),
+    FormBlock: dynamic(() => import('./blocks/FormBlock')),
+    GenericSection: dynamic(() => import('./sections/GenericSection')),
+    ImageBlock: dynamic(() => import('./blocks/ImageBlock')),
+    ImageGallerySection: dynamic(() => import('./sections/ImageGallerySection')),
+    PostFeedSection: dynamic(() => import('./sections/PostFeedSection')),
+    PricingSection: dynamic(() => import('./sections/PricingSection')),
+    RecentPostsSection: dynamic(() => import('./sections/RecentPostsSection')),
+    SelectFormControl: dynamic(() => import('./blocks/FormBlock/SelectFormControl')),
+    TextareaFormControl: dynamic(() => import('./blocks/FormBlock/TextareaFormControl')),
+    TextFormControl: dynamic(() => import('./blocks/FormBlock/TextFormControl')),
+    VideoBlock: dynamic(() => import('./blocks/VideoBlock')),
+    PageLayout: dynamic(() => import('./layouts/PageLayout')),
+    PostLayout: dynamic(() => import('./layouts/PostLayout')),
+    PostFeedLayout: dynamic(() => import('./layouts/PostFeedLayout')),
+    PostFeedCategoryLayout: dynamic(() => import('./layouts/PostFeedCategoryLayout'))
+};
+
+/**
+ * The getComponent() function loads a component using dynamic import.
+ */
+export function getComponent(key: string): ComponentType<any> {
+    return components[key];
+}
