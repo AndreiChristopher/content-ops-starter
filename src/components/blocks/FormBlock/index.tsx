a264ec3ead458f4d4da7521351c093886b833cbb
AndreiChristopher <33236539+AndreiChristopher@users.noreply.github.com>
2025-03-31 14:02:57 -0300

    Formulário simplificado #4

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -14,7 +14,7 @@ export function FeedbackForm() {
     await fetch("/__forms.html", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
+      body: new URLSearchParams(formData),
     });
     // Success and error handling ...
   };
