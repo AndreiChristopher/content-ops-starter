765825ac4b7b2556d6badc774bb2a2cd492eb522
AndreiChristopher <33236539+AndreiChristopher@users.noreply.github.com>
2025-03-31 16:01:39 -0300

    Form style

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -32,12 +32,61 @@ export default function FeedbackForm() {
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      {/* Campos do formulário */}
-    </form>
-  );
-}
-
+        <form
+            className={classNames(
+                'sb-component',
+                'sb-component-block',
+                'sb-component-form-block',
+                className,
+                styles?.self?.margin ? mapStyles({ margin: styles?.self?.margin }) : undefined,
+                styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined,
+                styles?.self?.borderWidth && styles?.self?.borderWidth !== 0 && styles?.self?.borderStyle !== 'none'
+                    ? mapStyles({
+                          borderWidth: styles?.self?.borderWidth,
+                          borderStyle: styles?.self?.borderStyle,
+                          borderColor: styles?.self?.borderColor ?? 'border-primary'
+                      })
+                    : undefined,
+                styles?.self?.borderRadius ? mapStyles({ borderRadius: styles?.self?.borderRadius }) : undefined
+            )}
+            name={elementId}
+            id={elementId}
+            onSubmit={handleSubmit}
+            ref={formRef}
+            data-sb-field-path= {fieldPath}
+        >
+            <div
+                className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
+                {...(fieldPath && { 'data-sb-field-path': '.fields' })}
+            >
+                <input type="hidden" name="form-name" value={elementId} />
+                {fields.map((field, index) => {
+                    const modelName = field.__metadata.modelName;
+                    if (!modelName) {
+                        throw new Error(`form field does not have the 'modelName' property`);
+                    }
+                    const FormControl = getComponent(modelName);
+                    if (!FormControl) {
+                        throw new Error(`no component matching the form field model name: ${modelName}`);
+                    }
+                    return <FormControl key={index} {...field} {...(fieldPath && { 'data-sb-field-path': `.${index}` })} />;
+                })}
+            </div>
+            {submitButton && (
+                <div className={classNames('mt-8', 'flex', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}>
+                    <SubmitButtonFormControl {...submitButton} {...(fieldPath && { 'data-sb-field-path': '.submitButton' })} />
+                </div>
+            )}
+        </form>
+    );
+  
+//  return (
+//    <form onSubmit={handleFormSubmit}>
+//      { Campos do formulário }
+//    </form>
+//  );
+//}
+ 
 /*export function FeedbackForm() {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
